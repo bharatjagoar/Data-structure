fb8ffc949d8e74a8b20b483d590c417af4e188e8
bharat jagoar <bharatjagoar15@gmail.com>
2024-04-20 00:03:48 +0530

    finding pivot again

--- lecture14/findPivot.cpp
+++ lecture14/findPivot.cpp
@@ -1,26 +1,41 @@
 #include<iostream>
 using namespace std;
 #include<bits/stdc++.h>
-int findpivot(vector<int>arr){
-    int low=0,high = arr.size()-1;
-    int mid = high+(low-high) / 2 ;
-    while (high>low)
+
+// hum yha pivot nikal rahe hai jiske lie hm arr[low] ko compare kareneg arr[mid]
+// vo islie kyuki hme check krna hai ki mid kha hai 
+// mid agar arr[low] se bda hai it means ki vo left sub array me hai 
+// else vo left sub array me hai 
+// agar vo left sub array me hai to hme start mid  krna hoga
+// else end ko mid krna hoga  
+// question ye kyu kya krna hai 
+// vo islie ki hum end me apna seach space kam krna chahte hai , or last me hmare pass ek hi element nikal kar aega
+// bs usi index ko age value number se compare krlo if vo chota it mean that array is rotated  
+
+int findPivotIndex(vector<int> arr){
+    // cout<<"helloo owrld";
+    int low = 0,high=arr.size()-1;
+    while (low<high)
     {
-        cout<<"hello world"<<endl;
+        int mid = low+(high-low)/2;
         if(arr[mid]>arr[low]){
-            low = mid+1;
+            low=mid;
         }else{
             high = mid;
         }
-        mid = high+(low-high)/2;
-    }
-    return low;
 
+    }
+    if(arr[low]>arr[low+1]){
+        return low+1;
+    }else{
+        return -1;
+    }
+    
+    // return index;
 }
 int main(){
-    // cout<<"hello world !";
-    vector<int> v1={4,5,6,7,0,1,2};
-    int pivot = findpivot(v1);
-    cout<<pivot;
+    vector<int> arr={6, 7, 8, 1, 2, 3, 4, 5};
+    int index=findPivotIndex(arr);
+    cout<<index;
     return 0;
 }
--- testcode.cpp
+++ testcode.cpp
@@ -1,8 +1,30 @@
 #include<iostream>
 using namespace std;
 #include<bits/stdc++.h>
+int findPivotIndex(vector<int> arr){
+    cout<<"helloo owrld";
+    int low = 0,high=arr.size()-1;
+    while (low<high)
+    {
+        int mid = low+(high-low)/2;
+        if(arr[mid]>arr[low]){
+            low=mid;
+        }else{
+            high = mid;
+        }
+
+    }
+    if(arr[low]>arr[low+1]){
+        return low+1;
+    }else{
+        return -1;
+    }
+    
+    // return index;
+}
 int main(){
-    int res =5+(4-5)/2;
-    cout<<res;
+    vector<int> arr={6, 7, 8, 1, 2, 3, 4, 5};
+    int index=findPivotIndex(arr);
+    cout<<index;
     return 0;
 }
