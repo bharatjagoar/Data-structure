c849e3b33fc4d41a7938f987e1d1b1e1555ff13d
bharat jagoar <bharatjagoar15@gmail.com>
2024-05-22 06:19:35 +0530

    bubble sort

--- lecture17/bubbleSort.cpp
+++ lecture17/bubbleSort.cpp
@@ -2,30 +2,26 @@
 using namespace std;
 #include<bits/stdc++.h>
 
-void Bubblesort(vector <int> *v1,int size){
-    
+void Bubblesort(vector <int> &arr,int size){
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            /* code */
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+        
+    }
     
 }
 
 
 int main(){
-    vector <int > v1 = {5,6,4,2,0,4,7};
+    vector <int> v1 = {66,1,4,2,0,4,7};
     int size = v1.size();
-    Bubblesort(&v1,size);
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
-            if(v1[j]>v1[j+1]){
-                swap(v1[j],v1[j+1]);
-            }
-        }
-        
-    }
-    for (int i = 0; i < size; i++)
-    {
-        cout<<v1[i]<<endl;
-    }
-    
+    Bubblesort(v1,size);
+    cout<<v1[0]<<endl;
     return 0;
 }
